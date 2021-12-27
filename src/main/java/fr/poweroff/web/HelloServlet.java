a925d384c3f07dcbc4f654adb05fc2ca23eaaa29
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2021-12-27 13:13:02 +0100

    feat: add BCrypt as password hasher

--- src/main/java/fr/poweroff/web/HelloServlet.java
+++ src/main/java/fr/poweroff/web/HelloServlet.java
@@ -1,6 +1,6 @@
 package fr.poweroff.web;
 
-import fr.poweroff.web.models.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Date;
-import java.sql.SQLException;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class  HelloServlet extends HttpServlet {
@@ -22,20 +20,8 @@ public class  HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Connection connection = DataBase.CONNECTION;
-
-        User user = User.create();
-        user.setEmail("test@test.fr");
-        user.setPasswordHash("ffffffff");
-        user.setLastname("Mmmm");
-        user.setFirstname("Mmmm");
-        user.setBorn(new Date(1321565476));
-        user.setLevel(0);
-        /*try {
-            user.save();
-        } catch (SQLException e) {
-            throw new IOException(e);
-        }*/
+        String password = "test";
+        String hash     = BCrypt.hashpw(password, BCrypt.gensalt());
 
         // Hello
         PrintWriter out = response.getWriter();
@@ -44,12 +30,12 @@ public class  HelloServlet extends HttpServlet {
         out.println("<a href=\"sign-up\">S'inscrire</a>");
         out.println("<a href=\"sign-in\">S'identifier</a>");
         out.println("<a href=\"sign-out\">Se deconnecter</a>");
-        out.println(user.getUserId());
-        /*try {
-            out.println(User.getFirst(user.getUserId()));
-        } catch (SQLException e) {
-            throw new IOException(e);
-        }*/
+
+        out.println("<h2>Password hash</h2>");
+        out.println("- plaintext: " + password + "<br>");
+        out.println("- hash: " + hash + "<br>");
+        out.println("- is equals: " + BCrypt.checkpw("test", hash));
+
         out.println("</body></html>");
     }
 
